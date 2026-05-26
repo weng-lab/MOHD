cc313ae20201b7bb5176ce3f61b1182f889f6f62
mlacadie <93140416+mlacadie@users.noreply.github.com>
2026-05-26 13:47:04 -0400

    Details Tabs (#54)
    * ome tabs
    
    * horizontal
    
    * remove file
    
    * comment

--- src/common/components/OmeDetails/OmeDetailsLayout.tsx
+++ src/common/components/OmeDetails/OmeDetailsLayout.tsx
@@ -1,82 +1,20 @@
 "use client";
 import { Box, Divider, Stack } from "@mui/material";
-import OmeDetailsTabs from "./OmeDetailsTabs";
-import { OmeDetailsTab, OmesDataType } from "@/common/types/globalTypes";
 import { OmeHeader } from "./OmeHeader";
 import { usePathname } from "next/navigation";
+import { DetailsTabs } from "@weng-lab/ui-components";
+import Link from "next/link";
+import { OME_TABS } from "@/common/components/OmeDetails/omeTabs";
+import type { OmesDataType } from "@/common/types/globalTypes";
 
 export type OmeDetailsLayoutProps = {
 } & { children: React.ReactNode };
-
-const logo = "/logo.png";
-const dimesnionalityReductionIcon = "/TabIcons/DimensionReduction.png";
-const cellTypeDecompositionIcon = "/TabIcons/CellTypeDeconvolution.png";
-const downloadIcon = "/TabIcons/DataDownload.png";
-const genomeBrowserIcon = "/TabIcons/genomeBrowser.png";
-const heatmapIcon = "/TabIcons/Heatmap.png";
-
-export const OME_TABS: Record<OmesDataType, OmeDetailsTab[]> = {
-    WGS: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Genetic Ancestry Composition", iconPath: logo, route: "ancestryComposition" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    WGBS: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Cell Type Decomposition", iconPath: cellTypeDecompositionIcon, route: "cellTypeDecomposition" },
-        { label: "Genome Browser", iconPath: genomeBrowserIcon, route: "genomeBrowser" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    ATAC: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Cell Type Decomposition", iconPath: cellTypeDecompositionIcon, route: "cellTypeDecomposition" },
-        { label: "Genome Browser", iconPath: genomeBrowserIcon, route: "genomeBrowser" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    RNA: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Cell Type Decomposition", iconPath: cellTypeDecompositionIcon, route: "cellTypeDecomposition" },
-        { label: "Genome Browser", iconPath: genomeBrowserIcon, route: "genomeBrowser" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    proteomics: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Heatmap", iconPath: heatmapIcon, route: "heatmap" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    metabolomics: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Heatmap", iconPath: heatmapIcon, route: "heatmap" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    lipidomics: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Heatmap", iconPath: heatmapIcon, route: "heatmap" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    exposomics: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-        { label: "Heatmap", iconPath: heatmapIcon, route: "heatmap" },
-        { label: "Downloads", iconPath: downloadIcon, route: "downloads" },
-    ],
-
-    metallomics: [
-        { label: "Dimensionality Reduction", iconPath: dimesnionalityReductionIcon, route: "dimensionalityReduction" },
-    ]
-};
 
 
 export default function OmeDetailsLayout({ children }: OmeDetailsLayoutProps) {
     const pathname = usePathname();
     const ome = pathname.split("/")[2] as OmesDataType;
-        
+
     const tabs = OME_TABS[ome];
 
     return (
@@ -97,14 +35,35 @@ export default function OmeDetailsLayout({ children }: OmeDetailsLayoutProps) {
                     maxHeight={"calc(100vh - 64px - var(--Ome-header-height, 66px))"}
                     display={{ xs: "none", md: "block" }}
                 >
-                    <OmeDetailsTabs ome={ome} tabs={tabs} orientation="vertical" />
+                    <DetailsTabs
+                        tabs={tabs}
+                        value={pathname.substring(pathname.lastIndexOf("/") + 1)}
+                        orientation="vertical"
+                        LinkComponent={Link}
+                        selectedBackgroundColor="#e1edec"
+                        sx={{
+                            position: "sticky",
+                            top: "calc(64px + var(--ome-header-height, 66px))",
+                            width: 100,
+                        }}
+                        iconHeight={50}
+                        iconWidth={50}
+                    />
                 </Box>
                 <Stack id="main-content" spacing={2} mx={2} mb={2} gridColumn={{ xs: 1, md: 2 }} gridRow={1}>
                     <Box id="horizonatal-view-tabs-container" display={{ xs: "block", md: "none" }}>
-                        <OmeDetailsTabs
-                            ome={ome}
+                        <DetailsTabs
                             tabs={tabs}
+                            value={pathname.substring(pathname.lastIndexOf("/") + 1)}
                             orientation="horizontal"
+                            LinkComponent={Link}
+                            selectedBackgroundColor="#e1edec"
+                            sx={{
+                                position: "sticky",
+                                top: "calc(64px + var(--ome-header-height, 66px))",
+                            }}
+                            iconHeight={40}
+                            iconWidth={40}
                         />
                         <Divider />
                     </Box>
