b98401e03a7327aaaeb251c8f03c0d6febb5f7bf
mlacadie <lacadiem14@gmail.com>
2026-06-09 11:28:10 -0400

    merge

--- src/app/omes/(multiomics)/ATAC/dimensionalityReduction/ATACUMAP.tsx
+++ src/app/omes/(multiomics)/ATAC/dimensionalityReduction/ATACUMAP.tsx
@@ -2,7 +2,7 @@ import { ATACMetadata, SharedATACDimenionalityProps } from "./page";
 import { Point, ScatterPlot, ChartProps } from "@weng-lab/visualization";
 import { useMemo, useState } from "react";
 import { sex_color_map, status_color_map, site_color_map, protocol_color_map } from "@/common/colors";
-import { Typography, Stack, SelectChangeEvent, Box } from "@mui/material";
+import { Typography, Stack, SelectChangeEvent, Box, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { ColorBySelect } from "@/common/components/ColorBySelect";
 import UMAPLegend from "@/common/components/UMAPLegend";
@@ -30,6 +30,8 @@ const ATACUMAP = <S extends true, Z extends boolean | undefined>({
 }: ATACDimensionalityUmapProps<S, Z>) => {
     const [colorScheme, setColorScheme] = useState<"sex" | "status" | "site" | "protocol">("site");
     const theme = useTheme();
+    const isXs = useMediaQuery(theme.breakpoints.down("md"));
+
 
     const { loading, data } = ATACData;
 
