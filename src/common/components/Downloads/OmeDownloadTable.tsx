9af2a77018d9517c14d643bf79d8ad466547b144
mlacadie <lacadiem14@gmail.com>
2026-06-09 10:49:23 -0400

    truncate table name

--- src/common/components/Downloads/OmeDownloadTable.tsx
+++ src/common/components/Downloads/OmeDownloadTable.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Tooltip } from "@mui/material";
+import { IconButton, Tooltip, Typography } from "@mui/material";
 import { Table } from "@weng-lab/ui-components";
 import { GridColDef, GridGroupingColDefOverride } from "@mui/x-data-grid-premium";
 import { useMemo } from "react";
@@ -265,11 +265,13 @@ export function OmeDownloadTable<T extends BaseSample>({
         },
     ];
 
+    const labelEl = <Typography noWrap>{label}</Typography>;
+
     return (
         <>
-            <DownloadToolbarProvider label={label}>
+            <DownloadToolbarProvider label={labelEl}>
                 <Table
-                    label={label}
+                    label={labelEl}
                     rows={expandedRows}
                     columns={columns}
                     loading={loading}
