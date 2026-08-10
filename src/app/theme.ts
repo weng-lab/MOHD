4bd73c9374f25b1517284d47f98128ef91ba915b
mlacadie <lacadiem14@gmail.com>
2026-08-10 13:26:56 -0400

    fix #43

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,5 +1,5 @@
 "use client";
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles"; 
 
 declare module "@mui/material/styles" {
   interface Palette {
